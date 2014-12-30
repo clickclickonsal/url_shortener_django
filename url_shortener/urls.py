c26f1652dc8373516e63d3488370077c79e28964
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-30 17:45:01 -0500

    added missing import in urls.py and 3rd test now passes

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from shorturls.views import LinkCreate
 
 urlpatterns = patterns('',
 	url(r'^$', LinkCreate.as_view(), name='home'),
