7c561383d870c3ea64ab1920e2e99a8aa808bae6
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-30 17:35:32 -0500

    created first view in views.py

--- shorturls/views.py
+++ shorturls/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Link
 
 # Create your views here.
+class LinkCreate(CreateView):
+	model = Link
+	fields = ["url"]
