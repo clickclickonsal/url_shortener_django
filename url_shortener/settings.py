b8475fa3f4d6f9e23f150db70a5f33e3beb4b59a
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-30 16:50:56 -0500

    named my database, added shortsapp in settings and ran syncdb command

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shorturls'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -59,7 +60,7 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'shorturls.db',
     }
 }
 
