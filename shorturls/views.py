daa0e3a0c2f5b9845b0355ab9d7dcb118e33e96c
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-30 17:56:43 -0500

    created LinkShow view

--- shorturls/views.py
+++ shorturls/views.py
@@ -1,7 +1,11 @@
 from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
 from .models import Link
 
 # Create your views here.
 class LinkCreate(CreateView):
 	model = Link
 	fields = ["url"]
+
+class LinkShow(DetailView):
+	model = Link
