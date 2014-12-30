ba62c51338fcadb4e6d95b8e7bf2e264ef6c6159
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-30 16:55:21 -0500

    created 1st test, to test that urls get shorter

--- shorturls/tests.py
+++ shorturls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .model import Link
 
 # Create your tests here.
+class ShortenerText(TestCase):
+	def test_shortens(self):
+		"""
+		Test that urls get shorter.
+		"""
+		url = "http://www.example.com/"
+		l = Link(url=url)
+		short_url = Link.shorten(1)
+		self.assertLess(len(short_url), len(url))
