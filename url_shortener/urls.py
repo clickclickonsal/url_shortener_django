b01fdd9a8c64e00ce8cfc1794169ab300bf07070
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-30 17:57:59 -0500

    created url for LinkShow view

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -4,4 +4,5 @@ from shorturls.views import LinkCreate
 
 urlpatterns = patterns('',
 	url(r'^$', LinkCreate.as_view(), name='home'),
+	url(r'^link/(?P<pk>\d+)$', LinkShow.as_view(), name='link_show'),
 )
