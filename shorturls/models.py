ab42f8500cb4087dfd65e590889269b190a1761c
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-30 17:17:43 -0500

    modified shorten method and added expand method to Link model

--- shorturls/models.py
+++ shorturls/models.py
@@ -7,4 +7,12 @@ class Link(models.Model):
 
 	@staticmethod
 	def shorten(long_url):
-		return "h"
+		l, _ = Link.objects.get_or_create(url=link.url)
+		return str(l.pk)
+
+	@staticmethod
+	def expand(slug):
+		link_id = int(slug)
+		l = Link.objects.get(pk=link_id)
+		return l.url
+		
