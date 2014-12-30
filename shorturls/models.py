cf90296deb30dd90cbddedbd7ec7ddde5c7f0e54
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-30 17:01:37 -0500

    created model and shorten method to pass 1st test

--- shorturls/models.py
+++ shorturls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Link(models.Model):
+	# Using this field is actually a Charfield but with a URL validator. AWESOME!
+	url = models.URLField()
+
+	@staticmethod
+	def shorten(long_url):
+		return "h"
