2bdc0754d70c641a630bcfb3686fde5a18812cc1
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-30 17:13:15 -0500

    created 2nd test to check that expanded url is the same as the original

--- shorturls/tests.py
+++ shorturls/tests.py
@@ -12,3 +12,16 @@ class ShortenerText(TestCase):
 		short_url = Link.shorten(1)
 		# checks if short_url length is less than url length.
 		self.assertLess(len(short_url), len(url))
+
+	def test_recover_link(self):
+		"""
+		Tests that shortened and expanded url is the same as original.
+		"""
+		url = "http://www.example.com/"
+		l = Link(url=url)
+		short_url = Link.shorten(l)
+		l.save()
+		# Another user asks for the expansion of short_url
+		exp_url = Link.expand(short_url)
+		# Checks that expanded url is equal to original url.
+		self.assertEqual(url, exp_url)
